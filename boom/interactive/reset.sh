5a6cebc1c8f88b5e0a22c805de13705c2a379cea
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2020-05-31 15:48:32 -0700

    Interactive test now accepts token through environment variable

--- boom/interactive/reset.sh
+++ boom/interactive/reset.sh
@@ -3,7 +3,7 @@
 curl -X PUT \
   -H "Accept: application/vnd.api+json" \
   -H "Content-Type: application/vnd.api+json" \
-  -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6ImYyYjkyZTYwLWM5ZTQtNDUxYi04MDQzLTdlMDAwMzc1ZmRhNyIsInN1YiI6ImYyYjkyZTYwLWM5ZTQtNDUxYi04MDQzLTdlMDAwMzc1ZmRhNyIsInNjcCI6InVzZXIiLCJhdWQiOm51bGwsImlhdCI6MTU5MDk1Mjk1MSwiZXhwIjoxNTkxMDM5MzUxLCJqdGkiOiI5ZjgxZjQxZC01ZDgxLTQyYmEtODg1MS0wOWE3NmFiYzdkM2EifQ.bYXz23DrYanTo5g0VCBavFt0qjCsQbvMJU7x8DfPN_4" \
+  -H "Authorization: Bearer $TOKEN" \
   --data-raw '{ "current": 1000 }' \
   'http://localhost:3001/v1/current'
 echo
@@ -11,6 +11,6 @@ echo
 curl -X PUT \
   -H "Accept: application/vnd.api+json" \
   -H "Content-Type: application/vnd.api+json" \
-  -H "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6ImYyYjkyZTYwLWM5ZTQtNDUxYi04MDQzLTdlMDAwMzc1ZmRhNyIsInN1YiI6ImYyYjkyZTYwLWM5ZTQtNDUxYi04MDQzLTdlMDAwMzc1ZmRhNyIsInNjcCI6InVzZXIiLCJhdWQiOm51bGwsImlhdCI6MTU5MDk1Mjk1MSwiZXhwIjoxNTkxMDM5MzUxLCJqdGkiOiI5ZjgxZjQxZC01ZDgxLTQyYmEtODg1MS0wOWE3NmFiYzdkM2EifQ.bYXz23DrYanTo5g0VCBavFt0qjCsQbvMJU7x8DfPN_4" \
+  -H "Authorization: Bearer $TOKEN" \
   'http://localhost:3001/v1/current?current=1000'
 echo
