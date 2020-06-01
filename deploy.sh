c841252445be5e5f3eabcf5f5431f04c5472f488
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2020-05-31 23:28:41 -0700

    Buildpack for heroku added in deploy file

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,5 @@ set -e
 set -a
 
 git subtree push --prefix boom heroku-boom master
+# Buildpack: https://github.com/mars/create-react-app-buildpack.git
 git subtree push --prefix poke heroku-poke master
