7b6b0e70fa840a6bf101433a1395c8747c605f39
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2020-05-31 23:32:55 -0700

    Deployment can now be forced

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,11 @@
 set -e
 set -a
 
-git subtree push --prefix boom heroku-boom master
-# Buildpack: https://github.com/mars/create-react-app-buildpack.git
-git subtree push --prefix poke heroku-poke master
+if [ "$FORCE" == 'on' ]; then
+  git push heroku-boom $(git subtree split --prefix boom master):master --force
+  git push heroku-poke $(git subtree split --prefix poke master):master --force
+else
+  git subtree push --prefix boom heroku-boom master
+  # Buildpack: https://github.com/mars/create-react-app-buildpack.git
+  git subtree push --prefix poke heroku-poke master
+fi
